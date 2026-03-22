f982c6b05fc46405bbcae37c5f28a240cc326c0d
alex <0alexgorbachev0@gmail.com>
2026-03-22 19:39:06 +0000

    Added more colour options, added more to the README, added new function to display days, program only a few things away from functional

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-## Blazer
+# Blazer
 
-A gui to track progress on anything - such as revision
+A gui to track progress on anything - such as revision, in a way to provide maximum customisation options to make it your own
 Designed to guilt trip you and keep you addicted to keeping up your productivity steak, with full customisibility
-File selection is kinda ugly, sorry about that had to use tkinter cos pygame dosent work as well for it on linux
+File selection is kinda ugly, sorry about that had to use tkinter cos pygame dosent work as well for it 
 ## Install
 ```bash
 # Recommended
@@ -21,3 +21,56 @@ pipx uninstall Blazer
 ```bash
 blzr # runs the program
 ```
+
+# Customisation
+Go into `config.json`, under the `USER` section, select the options you want to use for your Blazer app
+
+*For example*: 
+
+```json
+//one of my prefered setups
+  "USER": {
+    "colourscheme": "forest",   // Changing forest -> ocean to change the base pallet to be more blue
+    "background": "dark23",     // Changing dark23 -> solarized to change the background
+    "font": "notoserif",        // you probably get it by now
+    "fontsize": 40              // There arent presets for font size so you can change it yourself
+  },
+```
+
+
+
+# Boring documentatation for nerds about how the program works
+###### Dont get offended, you gotta be a certain level of nerd to wanna know how a specific program works
+## Getting the configuration
+
+The config is stored as a `.json` file,
+This is so there can be set presets without them needing to be hardcoded into the python, 
+personally id much rather edit a json than a python file for customisation
+
+so in python we need to
+```python
+import os
+import json
+
+CONF = os.path.join(os.path.dirname(__file__), 'config.json')
+# bunch of code
+ 
+with open(CONF, "r") as f:
+    config = json.load(f)
+
+userconfig = config['USER']
+
+COLOURS = tuple(config['colourschemes'][userconfig['colourscheme']]) # example of data gotten from USER
+# bunch of code 
+```
+The `CONF = [...]` line is to make sure were using the right config file, in the same folder as the program
+The data from that file is then saved as `config` using the json library
+Then the part we actually care about, `USER` is set as `userconfig` which is a list of the user choices.
+
+```python
+tuple() 
+```
+is used for making the list into an array () that is needed for certain pygame features () 
+
+The code then finds the user `colourscheme` in the `colourschemes` part of the json file and reads the list of colours as arrays
+A similar process is used for all the other parts of the user's config
--- blazer.py
+++ blazer.py
@@ -6,29 +6,38 @@ import datetime
 import tkinter as tk
 from tkinter import filedialog
 
-BASEDIR = os.path.dirname(__file__)
-CONF = os.path.join(BASEDIR, 'config.json')
+CONF = os.path.join(os.path.dirname(__file__), 'config.json')
 
+HEIGHT = 640
+WIDTH = 640
 
 pygame.init()
 
-screen = pygame.display.set_mode((640, 640))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Blazer")
 clock = pygame.time.Clock()
 
 with open(CONF, "r") as f:
     config = json.load(f)
 
-user = config['USER']
+userconfig = config['USER']
 
-COLOURS = tuple(config['colourschemes'][user['colourscheme']])
-LIGHTEST = COLOURS[3]
-LIGHT = COLOURS[2]
-MID = COLOURS[1]
-DARK = COLOURS[0]
+COLOURS = tuple(config['colourschemes'][userconfig['colourscheme']])
 
-BG = tuple(config['background'][user['background']])
-FONT = pygame.font.SysFont(config['font'][user['font']], user['fontsize'])
+LIGHTEST = COLOURS[4]
+LIGHT = COLOURS[3]
+MID = COLOURS[2]
+DARK = COLOURS[1]
+DARKEST = COLOURS[0]
+
+BGCOLOURS = tuple(config['background'][userconfig['background']])
+BGPRIMARY = BGCOLOURS[0]
+BGSECONDAY = BGCOLOURS[1]
+
+
+SMALLFONT = pygame.font.SysFont(config['font'][userconfig['font']], userconfig['fontsize'] - 20)
+FONT = pygame.font.SysFont(config['font'][userconfig['font']], userconfig['fontsize'])
+BIGFONT = pygame.font.SysFont(config['font'][userconfig['font']], userconfig['fontsize'] + 20)
 
 
 timeframeinmonths = 1
@@ -38,7 +47,7 @@ year = date[0]
 month = date[1]
 day = date[2]
 
-daysincurrentmonth = calendar.monthrange(int(year),int(month))[1]
+daysincurrentmonth = int(calendar.monthrange(int(year),int(month))[1])
 
 
 tkroot = tk.Tk()
@@ -50,8 +59,19 @@ def square(x, y, width, height, colour, border_radius):
 def text(font, text, colour):
     return font.render(text, True, colour)
 
-def formatmonth(days):
-    pass
+def displaypastdays(days):
+    gap = 10
+    step = 40 + gap
+    startx = WIDTH * 0.12
+    starty = HEIGHT * 0.3
+    squaresperrow = int((WIDTH * 0.8) // step)
+
+    for day in range(days):
+        row = day // squaresperrow
+        col = day % squaresperrow
+        x = startx + col * step
+        y = starty + row * step
+        square(x, y, 40, 40, DARKEST, 5)
 
 def main():
     filepath = None
@@ -70,16 +90,21 @@ def main():
                     )
                     print(filepath)
 
-        screen.fill(BG)
-        screen.blit(text(FONT, "Blazer", MID), (50, 25))
-        square(50, 100, 20, 20, DARK, 5)
-        square(80, 100, 20, 20, MID, 5)
-        square(110, 100, 20, 20, LIGHT, 5)
-        square(140, 100, 20, 20, LIGHTEST, 5)
+        screen.fill(BGPRIMARY)
+        screen.blit(text(BIGFONT, "Blazer", MID), (50, 20))
 
-        screen.blit(text(FONT, year, MID), (500,25))
-        screen.blit(text(FONT, month, MID), (500,55))
+        square(50, 100, 20, 20, DARKEST, 5)
+        square(80, 100, 20, 20, DARK, 5)
+        square(110, 100, 20, 20, MID, 5)
+        square(140, 100, 20, 20, LIGHT, 5)
+        square(170, 100, 20, 20, LIGHTEST, 5)
 
+        screen.blit(text(SMALLFONT, year, MID), (500,30))
+        screen.blit(text(SMALLFONT, month, MID), (500,55))
+
+        square(WIDTH*0.1,HEIGHT*0.2, WIDTH * 0.8, HEIGHT*0.6, BGSECONDAY, 5)
+
+        displaypastdays(daysincurrentmonth)
 
         pygame.display.flip()
         clock.tick(60)
